f1fe74e6e9c1e2a1956ae6b0e3f241d695627ccc
GaoXiangYa <gx19991105@outlook.com>
2025-12-16 08:17:56 +0800

    complete week6

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -1,5 +1,5 @@
-use crossbeam_channel;
-use std::{thread, time};
+use crossbeam_channel::{self, unbounded, Receiver, Sender};
+use std::{process::id, thread, time};
 
 fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
 where
@@ -9,6 +9,37 @@ where
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
     // TODO: implement parallel map!
+    let (tx1, rx1): (Sender<U>, Receiver<U>) = unbounded();
+    let (tx2, rx2): (Sender<U>, Receiver<U>) = unbounded();
+
+    for val in input_vec.into_iter() {
+        tx1.send(f(val))
+            .expect("tx1 send message failed!");
+    }
+
+    drop(tx1);
+
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let recv = rx1.clone();
+        let sender = tx2.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok(num) = recv.recv() {
+                sender.send(num).expect("tx2 send message failed");
+            }
+        }));
+    }
+
+    drop(tx2);
+
+    while let Ok(num) = rx2.recv() {
+        output_vec.push(num);
+    }
+
+    for t in threads {
+        t.join().expect("panic in thread");
+    }
+
     output_vec
 }
 
